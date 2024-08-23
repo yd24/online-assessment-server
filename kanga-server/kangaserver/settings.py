374a42aa289aaac481ac90c2d023e859220b2a1c
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2024-08-23 04:33:57 -0700

    Revert debug mode

--- kanga-server/kangaserver/settings.py
+++ kanga-server/kangaserver/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
