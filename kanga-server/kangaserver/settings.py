ada1cef6c99a42de4073aceaf99e4171f5eb93e7
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2024-08-23 03:58:28 -0700

    Adjust default to use sqlite properly in database settings

--- kanga-server/kangaserver/settings.py
+++ kanga-server/kangaserver/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'kangaserver.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default = os.path.join(BASE_DIR, 'db.sqlite3'),
+        default = 'sqlite:///db.sqlite3',
         conn_max_age=600,
         conn_health_checks=True,
     )
